6210fd02e67d93f96b899a4200898e72f115e413
Ying <yin.555@osu.edu>
2019-09-28 12:47:14 -0400

    load_fps paths added

--- src/PH_VR.m
+++ src/PH_VR.m
@@ -6,7 +6,7 @@ addpath(paths.in.ripser)
 % read mesh
 [T,X,Y,Z] = read_data(paths,fname);
 % read fps sample points
-I = load_fps(params,fname);
+I = load_fps(paths,params,fname);
 
 [dm,~] = distance_matrix(params,I,T,X,Y,Z);
 dgm = ripserDM(dm,2,2);
--- src/load_fps.m
+++ src/load_fps.m
@@ -1,9 +1,9 @@
-function I = load_fps(params,fname)
-global paths
+function I = load_fps(paths,params,fname)
 try
 %     load/create subsampled vertices by FPS
     load([paths.out.fps fname(1:end-4) '.mat'])
 catch
+    [~,X,Y,Z] = load_data(paths,fname);
     I = euclid_far_samp(X,Y,Z,params.numPoints);
     save([paths.out.fps fname(1:end-4) '.mat'],'I')
 end
